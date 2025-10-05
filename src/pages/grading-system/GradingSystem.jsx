f6ebe65df295166d9db825c5422fda344df65f40
franklin-raph <igboekwulusifranklin@gmail.com>
2025-10-05 15:12:50 +0100

    update

--- src/pages/grading-system/GradingSystem.jsx
+++ src/pages/grading-system/GradingSystem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SideNav from '../../components/side-nav/SideNav';
 import TopNav from '../../components/top-nav/TopNav';
 import { useNavigate } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { IoCloseOutline } from 'react-icons/io5';
 import BtnLoader from '../../components/btn-loader/BtnLoader';
 import { BiChevronDown } from 'react-icons/bi';
 import Alert from '../../components/alert/Alert';
+import { AiOutlineDelete } from 'react-icons/ai';
 
 const GradingSystem = ({baseUrl}) => {
 
@@ -16,7 +17,8 @@ const GradingSystem = ({baseUrl}) => {
     const [modal, setModal] = useState('');
     const [loading, setLoading] = useState(false)
     const [dropDown, setDropDown] = useState(false)
-    const [selectedTagApplies, setSelectedTagApplies] = useState('')
+    const [selectedId, setSelectedId] = useState('')
+    const [threashHoldValues, setThreashHoldValues] = useState()
     const [toggleNav, setToggleNav] = useState(false)
     const [monitorType, setMonitorType] = useState('monitorSource')
     const tags = ["Before expected start time", "At expected start time", "After expected start time"]
@@ -32,6 +34,16 @@ const GradingSystem = ({baseUrl}) => {
         {
             label: "Absent",
             value: 'absent'
+        }
+    ]
+    const tagType = [
+        {
+            label: "Monitor Source",
+            value: 'monitorSource'
+        },
+        {
+            label: "Monitor End",
+            value: 'monitorEnd'
         }
     ]
 
@@ -104,8 +116,63 @@ const GradingSystem = ({baseUrl}) => {
         }));
       };
 
+      async function getGradingSystem(){
+        setLoading(true)
+        const res = await fetch(`${baseUrl}/threshold-values`,{
+            method:"GET",
+            headers:{
+              "Content-Type":"application/json",
+              'Authorization':`Bearer ${user.data.access_token}`
+          }
+        })
+        const data = await res.json()
+        console.log(data);
+        
+        if(res) setLoading(false)
+        if(res.ok){
+          console.log(data);
+          setThreashHoldValues(data.data)
+          // setPerformanceValues(data.data);
+          return;
+        }
+        if(!res.ok){
+          setMsg(data.message);
+          setAlertType('error');
+          return;
+        }
+      }
 
-    
+      useEffect(() => {
+        getGradingSystem()
+      }, [])
+
+      async function deleteRecord(){
+        setLoading(true)
+        const res = await fetch(`${baseUrl}/threshold-value/${selectedId}`,{
+            method:"DELETE",
+            headers:{
+              "Content-Type":"application/json",
+              'Authorization':`Bearer ${user.data.access_token}`
+          }
+        })
+        const data = await res.json()
+        console.log(data);
+        
+        if(res) setLoading(false)
+        if(res.ok){
+          console.log(data);
+          setMsg(data.message);
+          setAlertType('success');
+          getGradingSystem()
+          setModal('')
+          return;
+        }
+        if(!res.ok){
+          setMsg(data.message);
+          setAlertType('error');
+          return;
+        }
+      }
 
   return (
     <div>
@@ -136,6 +203,7 @@ const GradingSystem = ({baseUrl}) => {
                                 <div className='flex bg-[#F2FCF8] px-2 py-2 rounded-full items-center gap-3'>
                                     <p className='bg-[#2C3933] text-[white] rounded-full py-2 px-4 cursor-pointer' onClick={() => setMonitorType('monitorSource')} >Staff</p>
                                     <p className='pr-2 cursor-pointer' onClick={() => setMonitorType('monitorEnd')} >Member</p>
+                                    <p className='pr-2 cursor-pointer' onClick={() => setMonitorType('records')} >Records</p>
                                 </div>
                             </header>
 
@@ -341,6 +409,7 @@ const GradingSystem = ({baseUrl}) => {
                                 <div className='flex bg-[#F2FCF8] px-2 py-2 rounded-full items-center gap-3'>
                                     <p className='pl-2 cursor-pointer' onClick={() => setMonitorType('monitorSource')} >Staff</p>
                                     <p className='bg-[#2C3933] text-[white] rounded-full py-2 px-4 cursor-pointer' onClick={() => setMonitorType('monitorEnd')} >Member</p>
+                                    <p className='pl-2 cursor-pointer' onClick={() => setMonitorType('records')} >Records</p>
                                 </div>
                             </header>
 
@@ -539,6 +608,72 @@ const GradingSystem = ({baseUrl}) => {
                         </div>
                     </div>
                 }
+
+                {
+                    monitorType === 'records' &&
+                    <div>
+                        <div className="py-6 px-[10px] lg:px-[30px] bg-white rounded-lg">
+                            <header className="mb-4 flex sm:items-center justify-between flex-col sm:flex-row gap-2">
+                                <h1 className="text-[##19201D]">Configure grading system</h1>
+                                <div className='flex bg-[#F2FCF8] px-2 py-2 rounded-full items-center gap-3'>
+                                    <p className='pl-2 cursor-pointer' onClick={() => setMonitorType('monitorSource')} >Staff</p>
+                                    <p className='pl-2 cursor-pointer' onClick={() => setMonitorType('monitorEnd')} >Member</p>
+                                    <p className='bg-[#2C3933] text-[white] rounded-full py-2 px-4 cursor-pointer' onClick={() => setMonitorType('records')} >Records</p>
+                                </div>
+                            </header>
+
+                            <section className="mb-4">
+                                <div class="relative overflow-x-auto mx-5 mt-10 py-8">
+                                    <table class="w-full text-sm text-left rtl:text-left">
+                                        <thead class="text-[13px] border-b">
+                                            <tr>
+                                                <th scope="col" class="px-6 py-3 th1 font-[700]">S/N</th>
+                                                <th scope="col" class="px-6 py-3 font-[700]">Type</th>
+                                                <th scope="col" class="px-6 py-3 font-[700]">Name</th>
+                                                <th scope="col" class="px-6 py-3 font-[700]">Max Value</th>
+                                                <th scope="col" class="px-6 py-3 font-[700]">Min Value</th>
+                                                <th scope="col" class="px-6 py-3 font-[700]">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        {
+                                            threashHoldValues && threashHoldValues?.map((item, index) => {
+                                                return (
+                                                    <tr className='text-[#19201D]'>
+                                                        <td className='px-6 py-2'>{index + 1}</td>
+                                                        <td className='px-6'>{item.type}</td>
+                                                        <td className='px-6'>{item.name}</td>
+                                                        <td className='px-6'>{item.maxVal}</td>
+                                                        <td className='px-6'>{item.minVal}</td>
+                                                        <td className='px-6'>
+                                                            <div className='flex items-center gap-3'>
+                                                                <button
+                                                                    onClick={() => {
+                                                                        setSelectedId(item._id)
+                                                                        setModal('edit')
+                                                                    }}
+                                                                >
+                                                                    <TbPencilDiscount className="w-5 h-5 text-gray-600 cursor-pointer" />
+                                                                </button>
+                                                                <button onClick={() => {
+                                                                    setSelectedId(item._id)
+                                                                    setModal('delete')
+                                                                }}>
+                                                                    <AiOutlineDelete className="hover:text-[#D61C4E] w-5 h-5 text-gray-600 cursor-pointer" />
+                                                                </button>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
         {
@@ -695,6 +830,112 @@ const GradingSystem = ({baseUrl}) => {
                 </div>
             </div>
         }
+        
+        {
+            modal === 'delete' &&
+            <div>
+                <div className="h-full w-full fixed top-0 left-0 z-[99]" style={{ background:"rgba(14, 14, 14, 0.58)" }} onClick={() => setModal(false)}></div>
+                <div className="bg-white sm:max-w-[650px] w-[95%] fixed top-[50%] left-[50%] pt-[20px] md:px-[2rem] px-[16px] z-[100] pb-[20px]" style={{ transform: "translate(-50%, -50%)" }}>
+                    <div className="flex items-center justify-between border-b pb-[5px]">
+                        <p className="text-[px]">Delete Record</p>
+                        <IoCloseOutline fontSize={"20px"} cursor={"pointer"} onClick={() => setModal(false)}/>
+                    </div>
+                    <div className='flex items-center flex-col gap-5 w-full px-5 py-[3rem] text-center'>
+                        <img src="/images/confirm.svg" alt="" />
+                        <p>Are you sure you want to delete this record.</p>
+                    </div>
+                    {
+                        loading ? 
+                        <BtnLoader bgColor="#191f1c"/>
+                        :
+                        <div className='flex items-center gap-5 justify-center'>
+                            <button onClick={() => setModal(false)} className=' border border-primary-color bg-white w-full rounded-[4px] mt-[2.5rem] px-[35px] py-[16px] text-center mx-auto'>Cancel</button>
+                            <button onClick={deleteRecord} className='text-white bg-primary-color w-full rounded-[4px] mt-[2.5rem] px-[35px] py-[16px] text-center mx-auto'>Confirm</button>
+                        </div>
+                    }
+                </div>
+            </div>
+        }
+        
+        {
+            modal === 'edit' &&
+            <div>
+                <div className="h-full w-full fixed top-0 left-0 z-[99]" style={{ background:"rgba(14, 14, 14, 0.58)" }} onClick={() => setModal(false)}></div>
+                <div className="bg-white sm:max-w-[650px] w-[95%] fixed top-[50%] left-[50%] pt-[20px] md:px-[2rem] px-[16px] z-[100] pb-[20px]" style={{ transform: "translate(-50%, -50%)" }}>
+                    <div className="flex items-center justify-between border-b pb-[5px]">
+                        <p className="text-[px]">Update Record</p>
+                        <IoCloseOutline fontSize={"20px"} cursor={"pointer"} onClick={() => setModal(false)}/>
+                    </div>
+                    <div className='mt-5'>
+                        <p>Select Tag Type</p>
+                        <div className='cursor-pointer border rounded-[4px] px-[6px] py-3 relative'>
+                            <div className='flex items-center justify-between' onClick={() => setDropDown(dropDown === 'tag-type' ? false : 'tag-type')}>
+                                <p className='capitalize px-3 rounded mt-1 w-full outline-none'>{type}</p>
+                                <BiChevronDown />
+                            </div>
+                            {
+                                dropDown === 'tag-type' &&
+                                <div className='rounded-[4px] border absolute left-0 top-[45px] bg-white z-[10] w-full'>
+                                    {
+                                        tagType?.map((type, index) => {
+                                            return (
+                                            <p className='text-[14px] p-2 hover:bg-gray-100 capitalize' onClick={() => {
+                                                setDropDown(false)
+                                                setType(type.value)
+                                            }}>{type.label}</p>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            }
+                        </div>
+                    </div>
+
+                    <div className='mt-5'>
+                        <p>Select Tag Name</p>
+                        <div className='cursor-pointer border rounded-[4px] px-[6px] py-3 relative'>
+                            <div className='flex items-center justify-between' onClick={() => setDropDown(dropDown === 'tag-name' ? false : 'tag-name')}>
+                                <p className='capitalize px-3 rounded mt-1 w-full outline-none'>{name}</p>
+                                <BiChevronDown />
+                            </div>
+                            {
+                                dropDown === 'tag-name' &&
+                                <div className='rounded-[4px] border absolute left-0 top-[45px] bg-white z-[10] w-full'>
+                                    {
+                                        tagName?.map((type, index) => {
+                                            return (
+                                            <p className='text-[14px] p-2 hover:bg-gray-100 capitalize' onClick={() => {
+                                                setDropDown(false)
+                                                setName(type.value)
+                                            }}>{type.label}</p>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            }
+                        </div>
+                    </div>
+                    <div className='mt-5'>
+                        <p>Range in percentage (Maximum)</p>
+                        <input type="text" className='outline-none w-full border border-gray py-2 px-2 rounded-[4px]' />
+                    </div>
+                    <div className='mt-5'>
+                        <p>Range in percentage (Minimum)</p>
+                        <input type="text" className='outline-none w-full border border-gray py-2 px-2 rounded-[4px]' />
+                    </div>
+                    {
+                        loading ? 
+                        <BtnLoader bgColor="#191f1c"/>
+                        :
+                        <div className='flex items-center gap-5 justify-center'>
+                            <button onClick={() => setModal(false)} className=' border border-primary-color bg-white w-full rounded-[4px] mt-[2.5rem] px-[35px] py-[16px] text-center mx-auto'>Cancel</button>
+                            <button onClick={deleteRecord} className='text-white bg-primary-color w-full rounded-[4px] mt-[2.5rem] px-[35px] py-[16px] text-center mx-auto'>Confirm</button>
+                        </div>
+                    }
+                </div>
+            </div>
+        }
+
         {
             msg && <Alert msg={msg} setMsg={setMsg} alertType={alertType}/>
         }
